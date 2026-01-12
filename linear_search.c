dad42d675f0ae6501a0ff980095d2dfa62dc2e4d
adeba4343-web <adeba4343@gmail.com>
2026-01-13 01:52:43 +0530

    Create linear_search.c

---
+++ linear_search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+  
+    int result = linearSearch(arr, n, key);
+
+    if (result != -1) {
+        printf("Element %d found at index %d\n", key, result);
+    } else {
+        printf("Element %d not found in the array\n", key);
+    }
+
+    return 0;
+}
