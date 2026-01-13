95a35fb7abe2a493d147a3b541885129567124d7
adeba4343-web <adeba4343@gmail.com>
2026-01-13 10:42:24 +0530

    Create doubly_linked_list.c

---
+++ doubly_linked_list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void insertEnd(int x) {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->prev = NULL;
+    newnode->next = NULL;
+
+    if (head == NULL)
+        head = newnode;
+    else {
+        struct node *temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newnode;
+        newnode->prev = temp;
+    }
+}
+
+void deleteEnd() {
+    if (head == NULL)
+        printf("List is empty\n");
+    else if (head->next == NULL) {
+        free(head);
+        head = NULL;
+    } else {
+        struct node *temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->prev->next = NULL;
+        free(temp);
+    }
+}
+
+void display() {
+    struct node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int ch, x;
+
+    while (1) {
+        printf("1.Insert 2.Delete 3.Display 4.Exit\n");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                scanf("%d", &x);
+                insertEnd(x);
+                break;
+            case 2:
+                deleteEnd();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+        }
+    }
+}
